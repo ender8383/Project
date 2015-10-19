32dfdf10c0fce0f253578208663900b2e2ed8d42
Michael Ashabraner <ender8383@live.com>
2015-10-18 22:26:40 -0400

    Made resume lightbox made jquery hover effect for media icons

---
+++ app.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+	var $overlay = $('<div title="click anywhere to exit" id="overlay"><iframe style="height: 75%;" margin="auto" height="80%" width="60%" name="plugin" type="application/pdf" id="pdf" src="PDF/My Resumé.pdf"></iframe></div>');
+	
+
+	$("body").append($overlay);
+		
+
+	$("#resume").click(function(event) {
+		event.preventDefault();
+		var resumeLocation = $(this).attr("href");
+				
+		$overlay.show();				
+	});
+
+
+	$('.social-icon2, .social-icon').mouseover(function() {
+	  $(this).css("opacity",".5");
+	  console.log("working")
+						
+	});
+	
+	$('.social-icon2, .social-icon').mouseleave(function() {
+		$(this).css("opacity","1");
+		
+	}); 	
+	
+	$overlay.click(function() {
+		$overlay.hide();
+	});
+	
+
+});
