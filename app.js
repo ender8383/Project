483b2f471f812dde60dd63c9b4f46b7f7acb0271
Michael Ashabraner <ender8383@live.com>
2015-11-04 06:15:14 -0500

    added exit hint for resume lightbox added a BETA full screen mode for the example adjusted margins to acomodate new div for exit text added a color-fade to the example box added click functionality to the input, not just keyup made input hint a little more clear
    added accessability
    improved page load speed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	
-	var $overlay = $('<div title="click anywhere to exit" id="overlay"><iframe style="height: 75%;" margin="auto" height="80%" width="60%" name="plugin" type="application/pdf" id="pdf" src="PDF/My Resumé.pdf"></iframe></div>');
-	
-
+        var $overlay = $('<div title="click anywhere to exit" id="overlay"><div class="exitButton" style="font-weight:bolder;color:white;margin-left:75%;font-size:2em;cursor:pointer;margin-top:35px;">EXIT</div><iframe style="height: 75%;" margin="auto" height="80%" width="60%" name="plugin" type="application/pdf" id="pdf" src="PDF/My Resumé.pdf"></iframe></div>');	
+		var $fullDiv = $('<div id="closeButton" title="Close". class="closeButton" style="left:85%;width:50px;height:50px;position:fixed;top:-6px;cursor:pointer;"><img src="img/tabletClose.png"></div>');
+		var $activated = $('<p><h1 style="color:rgba(9,9,9,0.5);left:0;top:0;"></h1></p>');		
+		
+		
 	$("body").append($overlay);
 		
 
@@ -29,5 +31,79 @@ $(document).ready(function(){
 		$overlay.hide();
 	});
 	
+	 $(".exitButton").on('click', function() {
+    
+        $overlay.hide();
+        });
+	
+	
+	$(".parsed").contents().find("body").append($activated);
+	$("body").append($fullDiv);
+	
+	
+	
+	
+	$activated.hide();
+	$fullDiv.hide();
+	
+	$( ".parsed" ).on( "click", function() {  
+		
+		$fullDiv.show();
+	  
+	    $( this ).css({
+	    'top' : '5%',
+	    'left' : '0',
+	    'right' : '0',
+	    'width' : '80%',
+	    'height' : '80%',
+	    'position' : 'fixed' 
+   
+});
+});
+
+$(document).ready(function(){	
+
+	$("body").on('click', '.closeButton', function() {
+
+		$(".parsed").css({
+		   'background-color' : '#fff',
+		   'color' : '#2e2e2e',
+		   'float' : 'right',
+		   'width' : '43%',
+		   'height' : '275px',
+		   'float' : 'right',
+		   'clear' : 'right',
+		   'position' : 'relative',
+		   'overflow' : 'scroll'   
+		 
+		});  
+		
+	    $fullDiv.hide();
+		$( this ).hide();
+
+
+	});
+});
+
+
+	
+
+$(document).ready(function(){	
+	
+	$(".source").one('mouseover', function() {
+	  
+		$(".parsed").animate({
+        backgroundColor: "#fff"
+		}, 1500 );
+			
+	
+	});	
+		
+	
+});	
+	
+	
+	
+
 	
 });
--- parse.js
+++ parse.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 	
-	$(".source").keyup(function(){
+	$(".source").on('click keyup' ,function(){
 		var parsed = $(".parsed");
-		parsed.contents().find("html").html($(".source").val());	
+		parsed.contents().find("html").html($(".source").val());
+			
 	});
 	
+	/* $( ".source" ).click(function() {
+		$( ".parsed" ).effect( "highlight", "orange" );
+	});
+	 */
 });
 
